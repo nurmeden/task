8da75b1a436272fb140cd43f27365b63595d02c9
nurmeden <nurmeden.02@gmail.com>
2023-06-15 17:23:28 +0500

    fix: Change the code

--- task3/task3.php
+++ task3/task3.php
@@ -1,58 +1,61 @@
 <?php
-function buildCommentTree($comments) {
+function GenerateHtml($comments) {
     $commentMap = array();
+    $html = '<ul>';
 
     foreach ($comments as $comment) {
-    $commentMap[$comment[0]] = $comment;
+        $commentMap[$comment[0]] = array(
+            'id' => $comment[0],
+            'parentId' => $comment[1],
+            'text' => $comment[2],
+            'children' => array()
+        );
     }
 
     foreach ($commentMap as $comment) {
-        $parentId = $comment[1];
-        if ($parentId != $comment[0]) {
-            if (isset($commentMap[$parentId])) {
-                $parentComment = $commentMap[$parentId];
-                    if (!isset($parentComment[3])) {
-                        $parentComment[3] = array();
-                    }
-                $parentComment[3][] = $comment;
-                $commentMap[$parentId] = $parentComment;
-            }
+        $parentId = $comment['parentId'];
+        if ($parentId != $comment['id'] && isset($commentMap[$parentId])) {
+            $commentMap[$parentId]['children'][] = &$commentMap[$comment['id']];
         }
-        }
+    }
 
-    $html = GenerateHtml($commentMap);
+    foreach ($commentMap as $comment) {
+        if ($comment['parentId'] == $comment['id']) {
+            $html .= '<li>' . $comment['text'];
+            if (!empty($comment['children'])) {
+                $html .= generateChildrenHtml($comment['children']);
+            }
+            $html .= '</li>';
+        }
+    }
+
+    $html .= '</ul>';
     return $html;
 }
 
-function GenerateHtml($comments) {
-$html = '<ul>';
-    foreach ($comments as $comment) {
-    $id = $comment[0];
-    $parentId = $comment[1];
-    $text = $comment[2];
-    $childComments = isset($comment[3]) ? $comment[3] : array();
-
-    $html .= '<li>' . $text;
-        if (!empty($childComments)) {
-        $html .= GenerateHtml($childComments);
+function generateChildrenHtml($children) {
+    $html = '<ul>';
+    foreach ($children as $child) {
+        $html .= '<li>' . $child['text'];
+        if (!empty($child['children'])) {
+            $html .= generateChildrenHtml($child['children']);
         }
         $html .= '</li>';
     }
     $html .= '</ul>';
-
     return $html;
 }
 
 $comments = array(
-    array(1, 1, "Comment 1"),
-    array(2, 1, "Comment 2"),
-    array(3, 2, "Comment 3"),
-    array(4, 1, "Comment 4"),
-    array(5, 2, "Comment 5"),
-    array(6, 3, "Comment 6"),
-    array(7, 7, "Comment 7")
+array(1, 1, "Comment 1"),
+array(2, 1, "Comment 2"),
+array(3, 2, "Comment 3"),
+array(4, 1, "Comment 4"),
+array(5, 2, "Comment 5"),
+array(6, 3, "Comment 6"),
+array(7, 7, "Comment 7")
 );
 
-$html = buildCommentTree($comments);
+$html = GenerateHtml($comments);
 echo $html;
 ?>
